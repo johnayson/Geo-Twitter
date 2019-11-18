132420e9d424ba474f01b14e0d1be40f0054a973
John <jdayson@ucdavis.edu>
2019-11-17 23:19:21 -0800

    Data clean up function

--- tweets_clean.py
+++ tweets_clean.py
@@ -1,2 +1,19 @@
 import twitter as tweets
+import re
+
+
+
 print(tweets.final_tweets)
+
+raw_tweets = tweets.final_tweets
+#Data Cleanup
+def clean_tweets(text):
+	#remove emojis
+	text = text.encode('ascii','ignore').decode('ascii')
+	#remove urls
+	text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+	return text
+
+for key in raw_tweets:
+	clean_tweet = clean_tweets(raw_tweets[key]['text'])
+	print(clean_tweet)
