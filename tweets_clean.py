e37f918c6a578e3e2b26ac23e6ac497abcbefa38
John <jdayson@ucdavis.edu>
2019-11-18 23:36:12 -0800

    raw and enriched files

--- tweets_clean.py
+++ tweets_clean.py
@@ -1,20 +1,34 @@
-import twitter as tweets
+#import twitter as tweets
 import re
+import json
+import glob
 
+#print(tweets.final_tweets)
 
-
-print(tweets.final_tweets)
-
-raw_tweets = tweets.final_tweets
+#raw_tweets = tweets.final_tweets
 #Data Cleanup
 def clean_tweets(text):
 	#remove emojis
-	#text = text.encode('ascii','ignore').decode('ascii')
+	text = text.encode('ascii','ignore').decode('ascii')
 	#remove urls
-	#text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+	text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
 	return text
+def dict_to_json(this_dict,name):
+	str_json = 'ref_' + name
+	with open(str_json, 'w') as outfile:
+                json.dump(this_dict, outfile, sort_keys=True, indent=4)
+		
+def refine(name):
+	with open(name) as data_file:    
+		data = json.load(data_file)
+		print(data)
+	for keys in data:
+		data[keys]['text'] = clean_tweets(data[keys]['text'])
+	print(data)
+	dict_to_json(data,name)
 
-for key in raw_tweets:
-	clean_tweet = clean_tweets(raw_tweets[key]['text'])
-	print(clean_tweet)
-	print("\n")
+
+
+refine('read.json')
+#files = glob.glob('*.json')
+#print(files)
--- twitter.py
+++ twitter.py
@@ -1,5 +1,9 @@
 import tweepy
 import os
+import json
+import time
+from datetime import datetime
+
 
 CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
 CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET') 
@@ -13,11 +17,17 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 teams = ['#NFL']
 tweets_count = 5
 
+def get_ts():
+	timestamp = int(time.time()*1000.0)
+	return timestamp
+def timestamp_date(ts):
+	ts = ts/1000
+	return datetime.fromtimestamp(ts)	
 
 # Create API object
 API = tweepy.API(auth)
 final_tweets = {}
-def getData():
+def get_data():
 	temp_list = set()
 	temp_dict = {}
 	for tweet in tweepy.Cursor(API.search, q = teams[0], result_type = "recent", tweet_mode = 'extended').items(100):
@@ -34,10 +44,20 @@ def getData():
 def main():
 	current_cnt = 0
 	while( current_cnt < tweets_count):
-		temp_dict = getData()
+		temp_dict = get_data()
 		current_cnt += len(temp_dict)
 		final_tweets.update(temp_dict)
 	print(current_cnt)
 	print(final_tweets)			
-
+	#json_tweets = json.dumps(final_tweets)
+	#print(json_tweets)
+	ts = get_ts()
+	json_file = 'tweets_' + str(ts) + '.json'
+	with open(json_file, 'w') as outfile:
+    		json.dump(final_tweets, outfile, sort_keys=True, indent=4)
+	
 main()
+ts = get_ts()
+print(ts)
+toDate = timestamp_date(ts)
+print(toDate)
