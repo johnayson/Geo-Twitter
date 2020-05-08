1700668658e4109e93df1871087ee763f6ed21fa
John <jdayson@ucdavis.edu>
2020-05-08 00:06:51 -0700

    Initial Kafka Consumer Version Done

--- kafka_consumer.java
+++ kafka_consumer.java
@@ -3,8 +3,43 @@ import java.util.Arrays;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+import com.mongodb.util.JSON;
 
+//Kafka consumer that listens live to the Stream
 public class kafka_consumer {
+   
+   public static void insert_mongodb ( String database, String col,String record){
+	
+	Mongo mongo = new Mongo("localhost", 27017);
+	//DB db = mongo.getDB("test");
+	DB db = mongo.getDB(database);
+	DBCollection collection = db.getCollection(col);	
+
+	//DBCollection collection = db.getCollection("tweets_samp");
+	//String json = "{'database' : 'mkyongDB','table' : 'hosting'," +
+	//  "'detail' : {'records' : 99, 'index' : 'vps_index1', 'active' : 'true'}}}";
+
+	DBObject dbObject = (DBObject)JSON.parse(record);
+
+	collection.insert(dbObject);
+
+	DBCursor cursorDocJSON = collection.find();
+	System.out.println("Print values in collection: "); 
+	while (cursorDocJSON.hasNext()) {
+		System.out.println(cursorDocJSON.next());
+	}
+	
+	System.out.println("End of Collection: ");
+       	
+   }
+
+
    public static void main(String[] args) throws Exception {
       //if(args.length == 0){
       //   System.out.println("Enter topic name");
@@ -33,14 +68,14 @@ public class kafka_consumer {
       //print the topic name
       System.out.println("Subscribed to topic " + topicName);
       int i = 0;
-      
       while (true) {
          ConsumerRecords<String, String> records = consumer.poll(100);
-         for (ConsumerRecord<String, String> record : records)
-         
+         for (ConsumerRecord<String, String> record : records) {
          // print the offset,key and value for the consumer records.
-         System.out.printf("offset = %d, key = %s, value = %s\n", 
-            record.offset(), record.key(), record.value());
+         	System.out.printf("offset = %d, key = %s, value = %s\n", 
+            	record.offset(), record.key(), record.value());
+		insert_mongodb("test","tweets_samp",record.value());
+	 }
       }
    }
 }
